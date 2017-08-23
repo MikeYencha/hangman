dc2273aad4030b564bbf4dd27f84bf86838520ba
MikeYencha <jmyeniii@icloud.com>
2017-08-22 23:33:12 -0400

    hangman so hard

---
+++ scripts/hangman.js
@@ -0,0 +1,71 @@
+// var x = 'foo'
+// console.log(x)
+// var guessLetter;
+// var lettersUsed;
+// var correctLetters;
+// var wrongLetters;
+// var letterUsedArray = [];
+// var i = 0;
+function game () {
+
+  var lose = 0
+  var guessLetter
+  var wordArray = ['death', 'crime', 'punishment', 'guilty', 'outlaw']
+  var gameWord = wordArray[Math.floor(Math.random() * wordArray.length)]
+  console.log(gameWord)
+  var gameLetters = gameWord.split('')
+  console.log(gameLetters)
+
+  for (var i = 0; i < gameLetters.length; i++) {
+    gameLetters[i] = '_'
+    var node = document.createElement("div")
+    var textnode = document.createTextNode(gameLetters[i])
+    node.appendChild(textnode)
+    console.log(gameLetters[i])
+    document.getElementById('word').appendChild(node)
+  }
+  document.onkeyup = function (event) {
+    guessLetter = event.key
+    console.log('onkeyup' + guessLetter)
+    alert("you've pressed " + guessLetter)
+    if (gameLetters.indexOf(guessLetter) !== -1) {
+      alert('your letter was found')
+      alert(guessLetter)
+      var x = document.getElementById('foo')
+      x.innerHTML = guessLetter
+    } else {
+      alert('your letter was not found')
+    }
+    for (var i = 0; i < guessLetter.length; i++) {
+      var b = guessLetter[i]
+      alert(guessLetter[i])
+      var s = document.getElementById('winDiv')
+      s.innerHTML = b
+    }
+  }
+}
+game()
+
+// function updateVegetablesCollection (veggies, veggie) {
+//     if (veggies.indexOf(veggie) === -1) {
+//         veggies.push(veggie);
+//         console.log('New veggies collection is : ' + veggies);
+//     } else if (veggies.indexOf(veggie) > -1) {
+//         console.log(veggie + ' already exists in the veggies collection.');
+//     }
+// }
+
+// var veggies = ['potato', 'tomato', 'chillies', 'green-pepper'];
+
+// updateVegetablesCollection(veggies, 'spinach');
+// New veggies collection is : potato,tomato,chillies,green-pepper,spinach
+// updateVegetablesCollection(veggies, 'spinach');
+// spinach already exists in the veggies collection.
+
+// for(i; i<gameLetters.length; i++) {
+//     if(guessLetter === gameLetters[i]) {
+//       console.log('you\'ve guessed a letter');
+//     } else {
+//       console.log('you suck')
+//     }
+// }
